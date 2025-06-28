a3ed5dd4e3078b957786c67d64ba9cc874f2562e
Henrique Azank dos Santos <102073359+Henrique-Azank@users.noreply.github.com>
2025-06-28 08:23:21 -0300

    feat: initial winit app example

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+// Implement the ApplicationHandler trait for our App struct.
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(event_loop.create_window(Window::default_attributes()).unwrap());
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            },
+            WindowEvent::RedrawRequested => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in AboutToWait, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+
+                // Draw.
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw in
+                // applications which do not always need to. Applications that redraw continuously
+                // can render here instead.
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
+
+fn main() -> Result<(), winit::error::OsError> {
+
+    // Instantiate a new event loop.
+    let event_loop = EventLoop::new().unwrap();
+
+    // ControlFlow::Wait pauses the event loop if no events are available to process.
+    // This is ideal for non-game applications that only update in response to user
+    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    // Instantiate a new application handler.
+    let mut app = App::default();
+
+    // Run the application with the event loop.
+    let result = event_loop.run_app(&mut app);
+
+    // Handle any errors that may have occurred during the application run.
+    if let Err(e) = result {
+        eprintln!("Error running the application: {}", e);
+    }
+
+    // Exit the application gracefully.
+    Ok(())
+}
